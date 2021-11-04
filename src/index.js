d73ac7cd794daa9ecf521556edeb0c63bdf7a864
mozmbutt <mozmbutt8@gmail.com>
2021-11-04 18:12:33 +0500

    JSX extention

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+function getButtonText(){
+    return 'hello'
+}
 //react components
 const App = () => {
-    return <div>Hello World !</div>;
-}
-
-const AppInput = () => {
-    return <input type="text" placeholder="name"/>;
+    const buttonText = 'Save'
+    console.log(buttonText);
+    return (
+        <div>
+            <h1>The form element</h1>
+            <label htmlFor="fname">First name:</label>
+            <input type="text" id="fname" name="fname"/>
+            <br/>
+            <label htmlFor="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"/>
+            <br/>
+            <input type="submit" value={ getButtonText() } />
+        </div>
+    )
 }
 
 
 //take react component and show it on screen
 ReactDom.render(
-    // <App />,
-    <AppInput />,
+    <App />,
     document.querySelector('#root')
 );
